1aca833ffaacfe0371316e14128f2768c248d1c4
Arthur Junqueira Cançado <arthur@MacBook-Pro-de-Arthur.local>
2013-10-08 10:02:13 -0300

    Initial Commit

---
+++ Denuncia/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Denuncia
+//
+//  Created by Arthur Junqueira Cançado on 08/10/13.
+//  Copyright (c) 2013 Arthur Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Denuncia/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Denuncia
+//
+//  Created by Arthur Junqueira Cançado on 08/10/13.
+//  Copyright (c) 2013 Arthur Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
